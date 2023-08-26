d10c8793faf0c6eddf7c620d1c65eb0ad5afcea0
Malú <130713333+malummartiins@users.noreply.github.com>
2023-08-26 16:25:05 -0300

    update Set

---
+++ CollectionsJava/src/Set/Pesquisa/AgendaContatos.java
@@ -0,0 +1,70 @@
+package Set.Pesquisa;
+
+import java.security.CodeSigner;
+import java.util.HashSet;
+import java.util.Set;
+
+public class AgendaContatos {
+    private Set<Contato> contatoSet;
+
+    public AgendaContatos() {
+        this.contatoSet = new HashSet<>();
+    }
+
+    public void adicionarContato(String nome, int numero) {
+        contatoSet.add(new Contato(nome, numero));
+    }
+
+    public void exibirContatos() {
+        System.out.println(contatoSet);
+    }
+
+    public Set<Contato> pesquisarPorNomes(String nome) {
+        Set<Contato> contatosPornome = new HashSet<>();
+        for (Contato c : contatoSet) {
+            if (c.getNome().startsWith(nome)) {
+                contatosPornome.add(c);
+            }
+        }
+        return contatosPornome;
+    }
+
+    public Contato atualizarNumeroContato(String nome, int novoNumero) {
+        Contato contatoAtualizado = null;
+        for (Contato c : contatoSet) {
+            if (c.getNome().equalsIgnoreCase(nome)) {
+                c.setNumero(novoNumero);
+                contatoAtualizado = c;
+                break;
+            }
+        }
+        return contatoAtualizado;
+    }
+
+    // testando
+
+    public static void main(String[] args) {
+        AgendaContatos agendaContatos = new AgendaContatos();
+
+        agendaContatos.adicionarContato("João", 123456789);
+        agendaContatos.adicionarContato("Maria", 987654321);
+        agendaContatos.adicionarContato("Maria Fernandes", 55555555);
+        agendaContatos.adicionarContato("Ana", 88889999);
+        agendaContatos.adicionarContato("Fernando", 77778888);
+        agendaContatos.adicionarContato("Carolina", 55555555);
+
+        agendaContatos.exibirContatos();
+
+        // Pesquisando contatos pelo nome
+        System.out.println(agendaContatos.pesquisarPorNomes("Maria"));
+
+        // Atualizando o número de um contato
+        Contato contatoAtualizado = agendaContatos.atualizarNumeroContato("Carolina", 44443333);
+        System.out.println("Contato atualizado: " + contatoAtualizado);
+
+        // Exibindo os contatos atualizados na agenda
+        System.out.println("Contatos na agenda após atualização:");
+        agendaContatos.exibirContatos();
+    }
+
+}
