7f96ea61e28492ea3456809a471f1540c38a5dea
Malú <130713333+malummartiins@users.noreply.github.com>
2023-08-26 15:32:34 -0300

    start set

---
+++ CollectionsJava/src/Set/OperacoesBasicas/Convidado.java
@@ -0,0 +1,30 @@
+package Set.OperacoesBasicas;
+
+public class Convidado {
+    private String nome;
+    private int codigoConvite;
+
+    
+    public Convidado(String nome, int codigoConvite) {
+        this.nome = nome;
+        this.codigoConvite = codigoConvite;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+
+    public int getCodigoConvite() {
+        return codigoConvite;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Convidado [nom e= " + nome + ", codigo do Convite=  " + codigoConvite + "]";
+    }
+
+    
+}
