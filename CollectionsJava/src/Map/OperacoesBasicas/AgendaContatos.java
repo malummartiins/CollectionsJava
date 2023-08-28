8ee5f9bb56749406da51aa47dfcd872a96339102
Malú <130713333+malummartiins@users.noreply.github.com>
2023-08-28 17:41:39 -0300

    update

---
+++ CollectionsJava/src/Map/OperacoesBasicas/AgendaContatos.java
@@ -0,0 +1,35 @@
+package Map.OperacoesBasicas;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AgendaContatos {
+    private Map<String, Integer> agendaContatoMap;
+
+    public AgendaContatos() {
+        this.agendaContatoMap = new HashMap<>();
+    }
+
+public void adicionarContato(String nome, Integer telefone){
+    agendaContatoMap.put(nome, telefone);
+}
+
+public void removerContato(String nome){
+    if (!agendaContatoMap.isEmpty()){
+        agendaContatoMap.remove(nome);
+    }
+}
+
+public void exibirContatos(){
+    System.out.println(agendaContatoMap);
+}
+
+public Integer pesquisarPorNome(String nome){
+    Integer numeroPorNome = null;
+    if (!agendaContatoMap.isEmpty()){
+        numeroPorNome = agendaContatoMap.get(nome);
+    }
+    return numeroPorNome;
+}
+    
+}
