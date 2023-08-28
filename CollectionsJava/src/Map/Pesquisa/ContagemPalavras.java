0512f4c4aaa4180f54c552a4cc4815b3bd6e6b68
Malú <130713333+malummartiins@users.noreply.github.com>
2023-08-28 20:32:47 -0300

    update Map

--- CollectionsJava/src/Map/Pesquisa/ContagemPalavras.java
+++ CollectionsJava/src/Map/Pesquisa/ContagemPalavras.java
@@ -14,16 +14,45 @@ public class ContagemPalavras {
         palavras.put(palavra, contagem);
     }
 
-    public void removerPalavra(String palvra){
-
+    public void removerPalavra(String palavra){
+        if (!palavras.isEmpty()){
+          palavras.remove(palavra);
+        }
     }
 
-    public void exibirContagemPalavras(){
-
+    public int exibirContagemPalavras(){
+        int contagemTotal = 0;
+        for (int contagem : palavras.values()){
+            contagemTotal += contagem;
+        }
+        return contagemTotal;
     }
 
-    public void encontrarPalavraMaisFrequente(){
-
+    public String encontrarPalavraMaisFrequente(){
+        String linguagemMaisFrequente = null;
+        int maiorContagem = 0;
+        for (Map.Entry<String, Integer> entry : palavras.entrySet()){
+            if (entry.getValue() > maiorContagem){
+                maiorContagem = entry.getValue();
+                linguagemMaisFrequente = entry.getKey();
+            } //pesquisar sobre entry
+        }
+        return linguagemMaisFrequente;
     }
-    
+    //TESTANDO
+    public static void main(String[] args) {
+        ContagemPalavras contagemLinguagens = new ContagemPalavras();
+         // Adiciona linguagens e suas contagens
+    contagemLinguagens.adicionarPalavra("Java", 2);
+    contagemLinguagens.adicionarPalavra("Python", 8);
+    contagemLinguagens.adicionarPalavra("JavaScript", 1);
+    contagemLinguagens.adicionarPalavra("C#", 6);
+
+    // Exibe a contagem total de linguagens
+    System.out.println("Existem " + contagemLinguagens.exibirContagemPalavras() + " palavras.");
+
+    // Encontra e exibe a linguagem mais frequente
+    String linguagemMaisFrequente = contagemLinguagens.encontrarPalavraMaisFrequente();
+    System.out.println("A linguagem mais frequente é: " + linguagemMaisFrequente);
+    }
 }
