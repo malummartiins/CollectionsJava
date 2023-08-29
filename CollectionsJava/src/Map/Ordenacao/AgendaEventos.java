9f84b0d1aed4fbdf6b5bd95a84565886fbcc7951
Malú <130713333+malummartiins@users.noreply.github.com>
2023-08-29 10:16:43 -0300

    Update

--- CollectionsJava/src/Map/Ordenacao/AgendaEventos.java
+++ CollectionsJava/src/Map/Ordenacao/AgendaEventos.java
@@ -1,8 +1,11 @@
 package Map.Ordenacao;
 
 import java.time.LocalDate;
+import java.time.Month;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
 
 public class AgendaEventos {
@@ -22,13 +25,45 @@ public class AgendaEventos {
         System.out.println(eventosTreeMap);
     }
 
-    public void obterProximoEvento(){
-        
+    public void obterProximoEvento() {
+        /*
+         * Set<LocalDate> dateSet = eventosMap.keySet();
+         * Collection<Evento> values = eventosMap.values();
+         */
+        // preciso que sempre que eu executar o método ele me de a data atual
+        LocalDate dataAtual = LocalDate.now();
+        Map<LocalDate, Evento> eventosTreeMap = new TreeMap<>(eventosMap);
+        for (Map.Entry<LocalDate, Evento> entry : eventosMap.entrySet()) {
+            if (entry.getKey().isEqual(dataAtual) || entry.getKey().isAfter(dataAtual)) {
+                System.out.println("O próximo evento: " + entry.getValue() + "acontecerá : " + entry.getKey());
+                break;
+            }
+        }
+
     }
 
     // TESTANDO
     public static void main(String[] args) {
         AgendaEventos agendaEventos = new AgendaEventos();
+
+        // Adiciona eventos à agenda
+        agendaEventos.adicionarEvento(LocalDate.of(2022, Month.JULY, 15), "Conferência de Tecnologia",
+                "Palestrante renomado");
+        agendaEventos.adicionarEvento(LocalDate.of(2022, 7, 9), "Workshop de Programação",
+                "Aula prática de desenvolvimento");
+        agendaEventos.adicionarEvento(LocalDate.of(2000, 1, 10), "Lançamento de Software",
+                "Demonstração da nova versão");
+        agendaEventos.adicionarEvento(LocalDate.of(2023, Month.AUGUST, 28), "Hackathon de Inovação",
+                "Competição de soluções criativas");
+        agendaEventos.adicionarEvento(LocalDate.of(2024, 9, 20), "Seminário de Inteligência Artificial",
+                "Discussão sobre IA avançada");
+
+        // Exibe a agenda completa de eventos
+        agendaEventos.exibirAgenda();
+
+        // Obtém e exibe o próximo evento na agenda
+        agendaEventos.obterProximoEvento();
+
     }
 
 }
