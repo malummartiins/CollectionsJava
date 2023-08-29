b5385e21c10a9c2561fddb28aa7a8d6e5ad7a8d6
Malú <130713333+malummartiins@users.noreply.github.com>
2023-08-29 09:49:13 -0300

    update Map e TreeMap

--- CollectionsJava/src/Map/Ordenacao/AgendaEventos.java
+++ CollectionsJava/src/Map/Ordenacao/AgendaEventos.java
@@ -1,5 +1,34 @@
 package Map.Ordenacao;
 
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class AgendaEventos {
-    
+    private Map<LocalDate, Evento> eventosMap;
+
+    public AgendaEventos() {
+        this.eventosMap = new HashMap<>();
+    }
+
+    public void adicionarEvento(LocalDate data, String nome, String atracao) {
+        Evento evento = new Evento(nome, atracao); // instanciei o evento
+        eventosMap.put(data, evento);
+    }
+
+    public void exibirAgenda() { // uso treeMap pq quero organizar em ordem crescente
+        Map<LocalDate, Evento> eventosTreeMap = new TreeMap<>(eventosMap);
+        System.out.println(eventosTreeMap);
+    }
+
+    public void obterProximoEvento(){
+        
+    }
+
+    // TESTANDO
+    public static void main(String[] args) {
+        AgendaEventos agendaEventos = new AgendaEventos();
+    }
+
 }
