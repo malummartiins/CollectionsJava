6489e2d608c57da8c07857057a5d6e187e50c3b9
Malú <130713333+malummartiins@users.noreply.github.com>
2023-08-29 10:40:44 -0300

    update final

---
+++ CollectionsJava/src/Map/Ordenacao/Livro.java
@@ -0,0 +1,31 @@
+package Map.Ordenacao;
+
+public class Livro {
+    private String titulo;
+    private String autor;
+    private double preco;
+
+    public Livro(String titulo, String autor, double preco) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.preco = preco;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro titulo = " + titulo + ", autor = " + autor + ", preço = " + preco;
+    }
+
+}
